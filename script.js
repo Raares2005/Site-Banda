4b5a9e55c5760470f29ac3b96fda6393ea04696d
Ream94 <94ream@gmail.com>
2022-01-24 19:44:07 +0200

    update minor ptr demonstratie

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let menuOpen = false;
 menuBtn.addEventListener('click', () =>{
     if(!menuOpen){
         menuBtn.classList.add('open');
-        $( ".burger" ).show();
+
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
-        $( ".burger" ).hide();
+
         menuOpen = false;
     }
 });
