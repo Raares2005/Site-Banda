32f86beadeca156324e08f57dc772d0a5732c15b
Raares2005 <rares.croitoru8@gmail.com>
2022-01-26 21:30:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const hamburger = document.querySelector('.menu-btn__burger')
-let hamburger = false;
-menuBtn.addEventListener('click', () =>{
-    if(!hamburger){
-        menuBtn.classList.add('open');
 
-        hamburger = true;
-    } else {
-        menuBtn.classList.remove('open');
-
-        hamburger = false;
-    }
-});
 
 
