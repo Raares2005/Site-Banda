97053a054d51381d733a2968b6f65ce22404c4d2
Raares2005 <rares.croitoru8@gmail.com>
2022-02-12 22:27:08 +0200

    membrii slider

---
+++ script.js
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+$(".slider-two")
+.not(".slick-initialized")
+.slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+
+});
